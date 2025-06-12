de8e2908f7a528ba6acbb76cf72652ed94687e73
David <david@vanschoorisse.nl>
2025-06-12 03:55:19 +0200

    added more log levels

--- Canon.Sdk/Logging/ILogger.cs
+++ Canon.Sdk/Logging/ILogger.cs
@@ -8,9 +8,11 @@ namespace Canon.Sdk.Logging
 {
     public interface ILogger
     {
+        void Verbose(string message);
         void Debug(string message);
         void Information(string message);
         void Warning(string message);
         void Error(string message, Exception ex = null);
+        void Fatal(string message, Exception ex = null);
     }
 }
